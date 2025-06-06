ca609b0d91140ad1e868640ad6cc7aa20c2d6f7d
Fernanda Vargas <fernandaaragaov@hotmail.com>
2025-06-06 21:08:57 +0200

    fix

--- src/ray/calculate_var.c
+++ src/ray/calculate_var.c
@@ -6,7 +6,7 @@
 /*   By: fvargas <fvargas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 13:32:35 by fvargas           #+#    #+#             */
-/*   Updated: 2025/06/06 18:44:26 by fvargas          ###   ########.fr       */
+/*   Updated: 2025/06/06 21:07:50 by fvargas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,12 @@ void	set_side_dist(t_x_y *side_dist, t_x_y dir_vec, t_x_y pos_player, \
 	double	y;
 
 	coor = get_updated_coor_player(pos_player, dir_vec, 1);
-	x = fabs(coor.x - pos_player.x) * delta_dist.x;
-	y = fabs(coor.y - pos_player.y) * delta_dist.y;
+	y = 1e10;
+	x = 1e10;
+	if (delta_dist.y != 1e10)
+		y = fabs(coor.y - pos_player.y) * delta_dist.y;
+	if (delta_dist.x != 1e10)
+		x = fabs(coor.x - pos_player.x) * delta_dist.x;
 	set_values_x_y(side_dist, x, y);
 }
 
@@ -39,10 +43,10 @@ void	set_delta_dist(t_x_y *delta_dis, t_x_y direction)
 {
 	t_x_y	delta;
 
-	delta.x = 1000000000; // THINK IS A HUGE NUMBER
+	delta.x = 1e10;
 	if (direction.x != 0)
 		delta.x = fabs(1 / direction.x);
-	delta.y = 1000000000; // THINK IS A HUGE NUMBER
+	delta.y = 1e10;
 	if (direction.y != 0)
 		delta.y = fabs(1 / direction.y);
 	set_values_x_y(delta_dis, delta.x, delta.y);

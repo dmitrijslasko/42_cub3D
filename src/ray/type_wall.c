82f6e0e4377e2d83cac96ed49fcec1287d70f79f
Fernanda Vargas <fernandaaragaov@hotmail.com>
2025-06-09 23:25:26 +0200

    distance and type wall correct

--- src/ray/type_wall.c
+++ src/ray/type_wall.c
@@ -11,3 +11,18 @@ t_type_wall	get_type_wall(char c, t_x_y direction)
 		return (EAST);
 	return (WEST);
 }
+
+void	print_ray(t_ray ray)
+{
+	printf("ray distance is %f \n", ray.dist);
+	printf("Wall type ");
+	if (ray.type_wall == NORTH)
+		printf("NORTH\n");
+	if (ray.type_wall == WEST)
+		printf("WEST\n");
+	if (ray.type_wall == EAST)
+		printf("EAST\n");
+	if (ray.type_wall == SOUTH)
+		printf("SOUTH\n");
+	printf("ray has %f percentage \n", ray.perc_img);
+}

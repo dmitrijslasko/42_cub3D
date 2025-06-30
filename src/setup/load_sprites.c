73bc7993ec1f52126aba18a74f8468a803b40d33
Dmitrijs Lasko <dmlasko@student.42berlin.de>
2025-06-30 20:44:15 +0200

    upd

--- src/setup/load_sprites.c
+++ src/setup/load_sprites.c
@@ -1,20 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_sprites.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dmlasko <dmlasko@student.42berlin.de>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/30 20:10:31 by dmlasko           #+#    #+#             */
+/*   Updated: 2025/06/30 20:10:31 by dmlasko          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
 typedef struct s_sprite_file
 {
 	const char	minimap_repr;
-	char	*filepath;
-	char	*filepath2;
+	char		*filepath;
+	char		*filepath2;
 }				t_sprite_file;
 
-static const t_sprite_file g_sprites[] =
-{
-	{'s', "./sprites/heart.xpm", "./sprites/heart.xpm"},
-	{'q', "./sprites/test.xpm", "./sprites/test.xpm"},
-	{'p', "./sprites/container.xpm", "./sprites/container.xpm"},
-	{'h', "./sprites/sammy1.xpm", "./sprites/sammy2.xpm"},
-	{'a', "./sprites/tommy1.xpm", "./sprites/tommy2.xpm"},
+static const t_sprite_file	g_sprites[] = {
+{'s', "./sprites/heart.xpm", "./sprites/heart.xpm"},
+{'q', "./sprites/test.xpm", "./sprites/test.xpm"},
+{'p', "./sprites/container.xpm", "./sprites/container.xpm"},
+{'h', "./sprites/sammy1.xpm", "./sprites/sammy2.xpm"},
+{'a', "./sprites/tommy1.xpm", "./sprites/tommy2.xpm"},
 };
+
+static int	set_sprite_img(t_data *dt, t_sprite_texture *texture,
+	size_t i, size_t frame)
+{
+	texture[i].sprite_img[frame] = mlx_xpm_file_to_image(\
+		dt->mlx_ptr, \
+		g_sprites[i].filepath, \
+		&texture[i].width, \
+		&texture[i].height);
+	return (EXIT_SUCCESS);
+}
 
 int	load_sprite_images(t_data *dt)
 {
@@ -27,30 +49,18 @@ int	load_sprite_images(t_data *dt)
 	while (i < dt->sprite_texture_count)
 	{
 		sprite_textures[i].type = g_sprites[i].minimap_repr;
-		sprite_textures[i].sprite_img[0] = mlx_xpm_file_to_image( \
-						dt->mlx_ptr, \
-						g_sprites[i].filepath, \
-						&sprite_textures[i].width, \
-						&sprite_textures[i].height);
-
-		sprite_textures[i].sprite_data[0] = (int *)mlx_get_data_addr( \
+		set_sprite_img(dt, sprite_textures, i, 0);
+		sprite_textures[i].sprite_data[0] = (int *)mlx_get_data_addr(\
 						sprite_textures[i].sprite_img[0], \
 						&sprite_textures[i].bpp, \
 						&sprite_textures[i].size_line, \
 						&sprite_textures[i].endian);
-
-		sprite_textures[i].sprite_img[1] = mlx_xpm_file_to_image( \
-						dt->mlx_ptr, \
-						g_sprites[i].filepath2, \
-						&sprite_textures[i].width, \
-						&sprite_textures[i].height);
-
-		sprite_textures[i].sprite_data[1] = (int *)mlx_get_data_addr( \
+		set_sprite_img(dt, sprite_textures, i, 1);
+		sprite_textures[i].sprite_data[1] = (int *)mlx_get_data_addr(\
 						sprite_textures[i].sprite_img[1], \
 						&sprite_textures[i].bpp, \
 						&sprite_textures[i].size_line, \
 						&sprite_textures[i].endian);
-
 		printf("Sprite image loaded!\n");
 		i++;
 	}
@@ -68,7 +78,8 @@ int	load_sprite_textures(t_data *dt)
 		return (EXIT_SUCCESS);
 	printf("Sprite elements found in the map: %zu\n", sprite_element_count);
 	printf("Sprite types found in the map: %zu\n", sprite_type_count);
-	dt->sprite_textures = protected_malloc(sizeof(t_sprite_texture) * sprite_type_count, dt);
+	dt->sprite_textures = protected_malloc(sizeof(t_sprite_texture) * \
+		sprite_type_count, dt);
 	dt->sprite_texture_count = sprite_type_count;
 	load_sprite_images(dt);
 	return (EXIT_SUCCESS);
@@ -77,7 +88,7 @@ int	load_sprite_textures(t_data *dt)
 int	load_sprites(t_data *dt)
 {
 	print_separator_default();
-	printf(TXT_YELLOW ">>> SPRITES\n" TXT_RESET);
+	printf(TXT_YELLOW ">>> LOADING SPRITES\n" TXT_RESET);
 	load_sprite_textures(dt);
 	find_all_sprites(dt);
 	printf(TXT_GREEN "Done!\n" TXT_RESET);

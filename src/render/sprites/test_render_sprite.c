50612e4d4daae41c2b6ab06bb1526c419ee47b53
Fernanda Vargas <fernandaaragaov@hotmail.com>
2025-06-30 17:37:47 +0200

    refactory test_render_sprite

--- src/render/sprites/test_render_sprite.c
+++ src/render/sprites/test_render_sprite.c
@@ -1,65 +1,76 @@
 #include "cub3d.h"
 
-bool	valid_sprites(t_data *dt, char type_sprite, t_sprite_texture *sprite_texture)
+bool	check_sprite_closer_than_wall(t_data *dt, t_coor *coor, t_sprite *spr)
 {
-	size_t	i;
+	float	distance_to_wall;
 
-	if (!dt->sprites || !dt->sprite_textures)
-		return (0);
-
-	i = 0;
-	while (i < dt->sprite_texture_count)
-	{
-		if (dt->sprite_textures[i].type == type_sprite)
-		{
-			*sprite_texture = dt->sprite_textures[i];
-			return (1);
-		}
-		i++;
-	}
+	distance_to_wall = dt->rays[coor->x / (WINDOW_W / CASTED_RAYS_COUNT)] \
+													.distance_to_wall;
+	if (distance_to_wall * distance_to_wall > spr->distance_to_player)
+		return (1);
 	return (0);
 }
 
+// unsigned int	find_color_texture(t_sprite_texture *texture, t_coor *coor, t_coor *offset, t_coor *sprite_size)
+// {
+// 	unsigned int	color;
+// 	int				tex_y;
+// 	int				tex_x;
+
+// 	tex_y = (coor->y - offset->y) * texture->height / sprite_size->y;
+// 	tex_x = (coor->x - offset->x) * texture->width / sprite_size->x;
+// 	color = *texture->sprite_data[tex_y * texture->width + tex_x];
+// 	return (color);
+// }
+
+// unsigned int	find_color_texture(t_sprite_texture *texture, t_coor *coor, t_coor *offset, t_coor *sprite_size, int time)
+// {
+// 	unsigned int	color;
+// 	int				tex_y;
+// 	int				tex_x;
+
+// 	tex_y = (coor->y - offset->y) * texture->height / sprite_size->y;
+// 	tex_x = (coor->x - offset->x) * texture->width / sprite_size->x;
+// 	color = texture->sprite_data[tex_y * texture->width + tex_x];
+// 	return (color);
+// }
+
+
 int	render_sprite(t_data *dt, t_sprite *sprite, int sprite_screen_x, float transform_y)
 {
-	//t_sprite_texture	sprite_textures;
 	unsigned int	color;
-	int				row;
-	int				col;
-	int 			time;
+	t_coor			coor;
+	t_coor			offset;
+	t_coor			sprite_size;
+	int				time;
 
 	time = (dt->last_time - dt->start_time) / 100 % 2;
-	int sprite_height = fmin(WINDOW_H * 4, WINDOW_H / transform_y);
-	int sprite_width = fmin(WINDOW_W * 4, sprite_height);
-	int offset_x = sprite_screen_x - sprite_width  / 2;
-	int offset_y = dt->view->screen_center_y - sprite_height / 3;
-
-	row = offset_y;
-	if (row < 0)
-		row = 0;
-	while (row < sprite_height + offset_y && row < WINDOW_H)
+	sprite_size.y = fmin(WINDOW_H * 4, WINDOW_H / transform_y);
+	sprite_size.x = fmin(WINDOW_W * 4, sprite_size.y);
+	offset.x = sprite_screen_x - sprite_size.x / 2;
+	offset.y = dt->view->screen_center_y - sprite_size.y / 3;
+	coor.y = ft_max(offset.y, 0);
+	while (coor.y < sprite_size.y + offset.y && coor.y < WINDOW_H)
 	{
-		col = offset_x;
-		if (col < 0)
-			col = 0;
-		while (col < sprite_width + offset_x && col < WINDOW_W)
+		coor.x = ft_max(offset.x, 0);
+		while (coor.x < sprite_size.x + offset.x && coor.x < WINDOW_W)
 		{
-			float distance_to_wall = dt->rays[col / (WINDOW_W / CASTED_RAYS_COUNT)].distance_to_wall;
-			if (distance_to_wall * distance_to_wall > sprite->distance_to_player)
+			if (check_sprite_closer_than_wall(dt, &coor, sprite))
 			{
-				int tex_y = (row - offset_y) * sprite->texture->height / sprite_height;
-				int tex_x = (col - offset_x) * sprite->texture->width / sprite_width;
+				int tex_y = (coor.y - offset.y) * sprite->texture->height / sprite_size.y;
+				int tex_x = (coor.x - offset.x) * sprite->texture->width / sprite_size.x;
 				color = sprite->texture->sprite_data[time][tex_y * sprite->texture->width + tex_x];
+				// color = find_color_texture(sprite->texture, &coor, &offset, &sprite_size);
 				if (color == TRANSPARENT_COLOR)
 				{
-					col++;
+					coor.x++;
 					continue;
 				}
-				img_pix_put(dt->scene_img, col, row, color);
+				img_pix_put(dt->scene_img, coor.x, coor.y, color);
 			}
-			col++;
+			coor.x++;
 		}
-		row++;
+		coor.y++;
 	}
 	return (EXIT_SUCCESS);
 }

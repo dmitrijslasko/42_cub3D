6ab5581349d152757283aa0ef7a009ec1f91f037
Fernanda Vargas <fernandaaragaov@hotmail.com>
2025-07-01 21:24:22 +0200

    render_ui_message

--- src/render/render_ui_message.c
+++ src/render/render_ui_message.c
@@ -3,9 +3,8 @@
 int	render_ui_message(t_data *dt)
 {
 	char			*src_pixel;
-	unsigned int	color;;
-	int				draw_x;
-	int 			draw_y;
+	unsigned int	color;
+	t_coor			draw;
 	int				row;
 	int				col;
 
@@ -15,13 +14,14 @@ int	render_ui_message(t_data *dt)
 		col = 0;
 		while (col < dt->message_img[0].width)
 		{
-			src_pixel = dt->message_img[0].addr + row * dt->message_img[0].line_len + col * dt->message_img->bpp / 8;
+			src_pixel = dt->message_img[0].addr + row * \
+				dt->message_img[0].line_len + col * dt->message_img->bpp / 8;
 			color = *(unsigned int *)src_pixel;
-			draw_x = ((WINDOW_W - dt->message_img[0].width)/ 2) + col;
-			draw_y = UI_MESSAGE_OFFSET_Y + row;
+			draw.x = ((WINDOW_W - dt->message_img[0].width) / 2) + col;
+			draw.y = UI_MESSAGE_OFFSET_Y + row;
 
 			if (color != TRANSPARENT_COLOR)
-				img_pix_put(dt->scene_img, draw_x, draw_y, color);
+				img_pix_put(dt->scene_img, draw.x, draw.y, color);
 			col++;
 		}
 		row++;

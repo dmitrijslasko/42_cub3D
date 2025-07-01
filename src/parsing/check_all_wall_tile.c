e0cf064433e7279e1d61971c66e38245f9266bbf
Fernanda Vargas <fernandaaragaov@hotmail.com>
2025-07-02 00:01:20 +0200

    wall need to be texture! && C/F need to be color

--- src/parsing/check_all_wall_tile.c
+++ src/parsing/check_all_wall_tile.c
@@ -6,7 +6,20 @@ bool	check_all_wall_tile(t_map *map)
 
 	i = 0;
 	while (i < NUMBER_TEXTURES)
-		if (map->wall_tile[i++].wall_type == DEFAULT_WALL)
-			return (error_msg("Error: Wall, floor or door N/D.", 0));
+	{
+		if (map->wall_tile[i].wall_type == DEFAULT_WALL)
+			return (error_msg("Error: Wall, floor or ceiling N/D.", 0));
+		if (i == NORTH || i == SOUTH || i == WEST || i == EAST)
+		{
+			if (map->wall_tile[i].is_color)
+				return (error_msg("Error: Walls need to be a texture.", 0));
+		}
+		else
+		{
+			if (!map->wall_tile[i].is_color)
+				return (error_msg("Error: Ceiling/Floor is not a color.", 0));
+		}
+		i++;
+	}
 	return (1);
 }
